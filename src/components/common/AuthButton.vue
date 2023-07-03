1c9e637e2b14d5f71725ad20f84020c68745dfda
ssing_7942 <djagmlwnn12@gmail.com>
2023-07-03 13:34:03 +0900

    로딩

--- src/components/common/AuthButton.vue
+++ src/components/common/AuthButton.vue
@@ -1,32 +1,51 @@
 <template>
     <div>
         <button @click="loginWithGoogle">Google 로그인</button>
-        <button @click="logout">Google 로그아웃</button>
+        <div v-if="loadingMessage !== ''">
+            <Loading v-if="loadingMessage !== ''" :message="loadingMessage" :startLoading="startLoading"
+                :stopLoading="stopLoading" />
+        </div>
     </div>
 </template>
 
 <script>
+import Loading from './../Loading.vue'
 import { onAuthStateChanged, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth'
 import { auth } from '../../firebase'
 import { ref, watchEffect } from 'vue'
+import { useStore } from 'vuex'
+import router from "../../router";
 
 export default {
+    components: {
+        Loading
+    },
     setup() {
+        const store = useStore()
         const user = ref(null)
+        const loadingMessage = ref('')
+
+        const startLoading = () => {
+            loadingMessage.value = '로딩 중입니다...'
+        }
+
+        const stopLoading = () => {
+            loadingMessage.value = ''
+        }
 
         // 사용자 인증 상태 변경 감지
         watchEffect(() => {
             onAuthStateChanged(auth, (currentUser) => {
+                startLoading()
                 if (currentUser) {
-                    
                     // 로그인 상태인 경우 처리할 코드 작성
                     user.value = currentUser
-                    console.log(user.value.accessToken, "hhh")
-                    localStorage.setItem("token", user.value.accessToken);
-                } else {
-                    // 로그아웃 상태인 경우 처리할 코드 작성
-                    user.value = null
+                    const token = currentUser.accessToken
+                    store.commit('SET_USER', user.value)
+                    store.commit('SET_TOKEN', token)
+                    localStorage.setItem("token", token);
                 }
+                stopLoading()
             })
         })
 
@@ -35,17 +54,11 @@ export default {
             signInWithRedirect(auth, provider)
         }
 
-        const logout = () => {
-            auth.signOut()
-                .then(() => {
-                    console.log('로그아웃되었습니다.')
-                })
-        }
-
         return {
-            user,
             loginWithGoogle,
-            logout
+            loadingMessage,
+            startLoading,
+            stopLoading
         }
     }
 }

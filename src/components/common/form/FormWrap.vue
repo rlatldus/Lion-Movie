61b8d91c06565417765a78d7523cc92383b3e1fa
ssing_7942 <djagmlwnn12@gmail.com>
2023-07-06 13:42:50 +0900

    좋아요 수정 필요, 구글 로그인 수정 필요, 폼 컴포넌트 수정필요

--- src/components/common/form/FormWrap.vue
+++ src/components/common/form/FormWrap.vue
@@ -2,19 +2,26 @@
     <main class="formWrap">
         <section class="forms">
             <slot></slot>
+            <span v-if="user">{{formText}} <router-link to="`${location}`"><span class="deco">{{loginout}}</span></router-link>{{formgider}}</span>
         </section>
+
     </main>
 </template>
 
 <script>
 export default {
-    name: 'FormWrap'
-};
+    name: 'FormWrap',
+    props: {
+        user: Boolean,
+  },
+}
+
 </script>
 
 
 <style  lang="scss" scoped>
 .formWrap {
+    margin-left: 50px;
     position: relative;
     top: 0;
     right: 0;
@@ -24,6 +31,7 @@ export default {
     background-size: cover;
     @media (max-width : 549px) {
 		position: absolute;
+        margin-left: 0px;
 	}
     .forms {
         display: flex;
--- src/components/views/Register.vue
+++ src/components/views/Register.vue
@@ -1,7 +1,6 @@
 <template>
   <FormWrap>
     <user-form formTitle="회원가입" submitButtonText="화원가입하기" :formData="formData" :submitForm="signUp"></user-form>
-    <span>만약 계정이 있다면, <router-link to="/login">로그인</router-link>페이지로 이동해주세요</span>
   </FormWrap>
 </template>
 
